711dffa459947e70ca9435f65849c5a78695dd22
Tiago Castro <tiagocastro1000@gmail.com>
2024-05-21 22:26:41 +0100

    commit deploy

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/f301.app/appvuej" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/f301.app/" : "/",
 };
