c5981b6a2c2dbcedf2bac81bc0995c67fa7dbe5d
Tiago Castro <tiagocastro1000@gmail.com>
2024-05-28 21:24:59 +0100

    a

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import App from '../App.vue'
+import App from './App.vue'
 import router from './router.js'; // Correct import path
 import './css/bootstrap.min.css';
 import './css/style.css';
